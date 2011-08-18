0e04d968879950251fe4897456f5f3d1c5b27143
aj heller <aj@drfloob.com>
2011-08-18 01:04:48 -0700

    submit 1

---
+++ ticket-lottery/ticketlottery.py
@@ -0,0 +1,26 @@
+import sys, math
+from math import factorial as fac
+
+def nCr(n,r):
+    return fac(n) / (float(fac(r)) * fac(n-r))
+
+def hyperGeo(goodWays, badWays, totalWinners, numberOfWins):
+    if numberOfWins > goodWays:
+        return 0
+    return (nCr(goodWays, numberOfWins) * nCr(badWays, totalWinners - numberOfWins)) / nCr(goodWays + badWays, totalWinners)
+
+def go(arr):
+    players, maxWinners, tickets, group = arr
+    minGWinners = int(math.ceil(group/float(tickets)))
+    if minGWinners > maxWinners:
+        return 0
+
+    prob = 0;
+    for i in xrange(minGWinners, maxWinners):
+        prob += hyperGeo(group, players - group, maxWinners, i)
+    
+    return prob
+
+if __name__ == '__main__':
+    for line in sys.stdin:
+        print go(map(int, line.strip().split(' ')))
