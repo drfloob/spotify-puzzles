f90460bb03c03cfddad621507d9a7a135312f3aa
aj heller <aj@drfloob.com>
2011-08-18 01:41:55 -0700

    from float to Decimal; try #4

--- ticket-lottery/ticketlottery.py
+++ ticket-lottery/ticketlottery.py
@@ -1,23 +1,25 @@
 import sys, math
 from math import factorial as fac
+from decimal import *
 
 def nCr(n,r):
-    return fac(n) / (float(fac(r)) * fac(n-r))
+    return Decimal(fac(n) / (fac(r) * fac(n-r)))
 
 def hyperGeo(goodWays, badWays, totalWinners, numberOfWins):
     if numberOfWins > goodWays:
         return 0
     if totalWinners - numberOfWins > badWays:
         return 0
+
     return (nCr(goodWays, numberOfWins) * nCr(badWays, totalWinners - numberOfWins)) / nCr(goodWays + badWays, totalWinners)
 
 def go(arr):
     players, maxWinners, tickets, group = arr
-    minGWinners = int(math.ceil(group/float(tickets)))
+    minGWinners = int(math.ceil(group/tickets))
     if minGWinners > maxWinners:
         return 0
 
-    prob = 0;
+    prob = Decimal(0);
     for i in xrange(minGWinners, maxWinners+1):
         prob += hyperGeo(group, players - group, maxWinners, i)
     
@@ -25,4 +27,4 @@ def go(arr):
 
 if __name__ == '__main__':
     for line in sys.stdin:
-        print '{0:.10f}'.format(go(map(int, line.strip().split(' '))))
+        print '{0:.10f}'.format(go(map(Decimal, map(int, line.strip().split(' ')))))
