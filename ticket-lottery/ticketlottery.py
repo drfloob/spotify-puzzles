7f1a832858afeffbdf31184aa35201bb7d45ed4a
Martin-Louis Bright <martin.louis.bright@autodesk.com>
2012-02-12 21:56:05 -0500

    ints

--- ticket-lottery/ticketlottery.py
+++ ticket-lottery/ticketlottery.py
@@ -18,11 +18,11 @@ def nCr(n, k):
 
 def hyperGeo(goodWays, badWays, totalWinners, numberOfWins):
     if numberOfWins > goodWays:
-        return 0
+        return Decimal(0)
     if totalWinners - numberOfWins > badWays:
-        return 0
+        return Decimal(0)
 
-    return (nCr(goodWays, numberOfWins) * nCr(badWays, totalWinners - numberOfWins)) / nCr(goodWays + badWays, totalWinners)
+    return Decimal(nCr(goodWays, numberOfWins) * nCr(badWays, totalWinners - numberOfWins)) / nCr(goodWays + badWays, totalWinners)
 
 def go(arr):
     players, maxWinners, tickets, group = arr
@@ -32,10 +32,10 @@ def go(arr):
 
     prob = Decimal(0);
     for i in xrange(minGWinners, min(int(maxWinners),int(group))+1):
-        prob += hyperGeo(group, players - group, maxWinners, i)
+        prob += hyperGeo(int(group), int(players - group), int(maxWinners), int(i))
     
     return prob
 
 if __name__ == '__main__':
     for line in sys.stdin:
-        print '{0:.10f}'.format(go(map(Decimal, map(int, line.strip().split(' ')))))
+        print '{0:.10f}'.format(go(map(Decimal, map(int, line.strip().split()))))
