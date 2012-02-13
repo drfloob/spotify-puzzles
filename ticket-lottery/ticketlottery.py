703fe0f387b1aa1975920543a3042d1877659580
Martin-Louis Bright <martin.louis.bright@autodesk.com>
2012-02-12 21:44:22 -0500

    ints

--- ticket-lottery/ticketlottery.py
+++ ticket-lottery/ticketlottery.py
@@ -31,7 +31,7 @@ def go(arr):
         return Decimal(0)
 
     prob = Decimal(0);
-    for i in xrange(minGWinners, min(maxWinners,int(group))+1):
+    for i in xrange(minGWinners, min(int(maxWinners),int(group))+1):
         prob += hyperGeo(group, players - group, maxWinners, i)
     
     return prob
